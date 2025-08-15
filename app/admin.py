c36687776f76b0c083d99db88f03da16bd359b15
AaritKumar <aaritkumar24@gmail.com>
2025-08-14 21:52:00 -0500

    Added Model to Admin

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import UserTracker
+from .models import UserTracker, DeviceTracker
 
 class UserTrackerAdmin(admin.ModelAdmin):
     list_display = ['user_id', 'total_devices', 'total_co2', 'total_kwh']
     search_fields = ['user_id__username']
 
+class DeviceTrackerAdmin(admin.ModelAdmin):
+    list_display = ['device_name', 'user', 'device_co2', 'device_kwh']
+    search_fields = ['device_name', 'user__username']
+
 admin.site.register(UserTracker, UserTrackerAdmin)
+admin.site.register(DeviceTracker, DeviceTrackerAdmin)
